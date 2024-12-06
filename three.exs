db20f008f1545066bf4d2faa0ae5aaf584ee3185
Mikel Valhondo <mvalhondo96@gmail.com>
2024-12-06 02:34:10 +0100

    second part

--- three.exs
+++ three.exs
@@ -1,18 +1,45 @@
 defmodule Three do
   Code.require_file("inputs.exs")
+
   @regexp ~r/mul\(\d{1,3},\d{1,3}\)/
+  @refined_regexp ~r/(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))/
 
   def scan do
     input = Inputs.three()
 
-    result =
-      @regexp
-      |> Regex.scan(input)
-      |> Enum.flat_map(& &1)
-      |> Enum.map(&(String.replace(&1, ["mul(", ")"], "") |> String.split(",") |> multiply()))
-      |> Enum.sum()
+    @regexp
+    |> Regex.scan(input)
+    |> Enum.flat_map(& &1)
+    |> Enum.map(&(String.replace(&1, ["mul(", ")"], "") |> String.split(",") |> multiply()))
+    |> Enum.sum()
+    |> IO.puts()
+  end
 
-    IO.puts(result)
+  def refined_scan do
+    input = Inputs.three()
+
+    @refined_regexp
+    |> Regex.scan(input, capture: :first)
+    |> Enum.flat_map(& &1)
+    |> Enum.reduce({[], nil}, fn action, {valids, check} ->
+      cond do
+        action in ["do()", "don't()"] ->
+          {valids, action}
+
+        is_nil(check) ->
+          {valids ++ [action], check}
+
+        check == "do()" ->
+          {valids ++ [action], check}
+
+        check == "don't()" ->
+          {valids, check}
+      end
+    end)
+    |> elem(0)
+    |> Enum.map(&(String.replace(&1, ["mul(", ")"], "") |> String.split(",") |> multiply()))
+    |> Enum.sum()
+    |> IO.puts()
   end
 
   defp multiply([first, second]) do
@@ -21,3 +48,4 @@ defmodule Three do
 end
 
 Three.scan()
+Three.refined_scan()
